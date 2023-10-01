89589d787ddf6c1143d27a6c73d0c21a0caac651
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-10-01 13:31:40 +0300

    base commit

---
+++ conftest.py
@@ -0,0 +1,11 @@
+# import pytest
+# from selenium import webdriver
+#
+#
+# @pytest.fixture
+# def driver_run():
+#     driver = webdriver.Chrome()
+#     driver.get('https://stellarburgers.nomoreparties.site/')
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
