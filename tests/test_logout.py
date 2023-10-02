92b441207b2dc465fa06aa9ae908819c9d76622d
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-10-02 13:08:40 +0300

    add test_logout.py

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -3,15 +3,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-class TestStellarBurgersAccount:
-    def test_go_to_constructor_login_user(self, driver):
+class TestStellarBurgersLogout:
+    def test_logout(self, driver):
         WebDriverWait(driver, 3).until(
             EC.element_to_be_clickable((By.XPATH, "//button[text()='Войти в аккаунт']"))).click()
         driver.find_element(By.XPATH, "//input[@name='name']").send_keys('evgenykoloskov1777@yandex.ru')
         driver.find_element(By.XPATH, "//input[@name='Пароль']").send_keys('230315')
         driver.find_element(By.XPATH, "//button[text()='Войти']").click()
         WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.LINK_TEXT, "Личный Кабинет"))).click()
-        driver.find_element(By.XPATH, "//p[text()='Конструктор']").click()
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Выход']"))).click()
         text = WebDriverWait(driver, 3).until(
-            EC.element_to_be_clickable((By.XPATH, "//h1[text()='Соберите бургер']"))).text
-        assert text == "Соберите бургер"
+            EC.element_to_be_clickable((By.XPATH, "//button[text()='Войти']"))).text
+        assert text == "Войти"
